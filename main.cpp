0dd86827932aa728e8a522a58016f21c0e4eec3c
Ivan <Barion2018@rambler.ru>
2018-10-06 10:31:02 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <sstream>
+#include <stack>
+#include <math.h>
+
+int main()
+{
+    std::stack<long long> args;
+
+    std::string line;
+    getline(std::cin, line);
+
+    long long number;
+    for(std::istringstream stream(line); stream >> number; ) {
+        args.push(number);
+    }
+
+    std::cout.setf(std::ios::fixed);
+    std::cout.precision(4); // число символов после точки
+
+    while (!args.empty()) {
+        std::cout << sqrt(args.top()) << std::endl;
+        args.pop();
+    }
+
+    return 0;
+}
+
+
