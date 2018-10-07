165d6cc09e3f7557eea25623d04e245ac5d1995e
Ivan <Barion2018@rambler.ru>
2018-10-07 17:32:59 +0500

    approved by timus

--- main.cpp
+++ main.cpp
@@ -1,31 +1,67 @@
 #include <iostream>
-#include <sstream>
+#include <fstream>
+#include <vector>
 #include <stack>
+#include <sstream>
+
 #include <math.h>
 
-//test
-
-int main()
+// ("",  '.') -> [""]
+// ("11", '.') -> ["11"]
+// ("..", '.') -> ["", "", ""]
+// ("11.", '.') -> ["11", ""]
+// (".11", '.') -> ["", "11"]
+// ("11.22", '.') -> ["11", "22"]
+std::vector<std::string> split(const std::string &str, char d)
 {
-    std::stack<long long> args;
+    std::vector<std::string> r;
 
-    std::string line;
-    getline(std::cin, line);
+    std::string::size_type start = 0;
+    std::string::size_type stop = str.find_first_of(d);
+    while(stop != std::string::npos)
+    {
+        r.push_back(str.substr(start, stop - start));
 
-    long long number;
-    for(std::istringstream stream(line); stream >> number; ) {
-        args.push(number);
+        start = stop + 1;
+        stop = str.find_first_of(d, start);
     }
 
-    std::cout.setf(std::ios::fixed);
-    std::cout.precision(4); // число символов после точки
+    r.push_back(str.substr(start));
 
-    while (!args.empty()) {
-        std::cout << sqrt(args.top()) << std::endl;
-        args.pop();
-    }
+    return r;
+}
+
+int main(int argc, char *argv[])
+{
+        std::string line;
+
+        std::vector<std::string> ip_clean;
+        while (getline(std::cin, line))
+        {
+            std::vector<std::string> v = split(line, ' ');
+            for(std::string str: v){
+                if(str == "") continue;
+                ip_clean.push_back(str);
+            }
+
+        }
+
+        std::stack<long long> args;
+        long long number;
+        for(std::string str: ip_clean){
+            std::istringstream stream(str);
+            stream >> number;
+            //number = std::stoi(str, nullptr, 0); // РґР»СЏ long long  -С…СЂРµРЅСЊ! РїРѕСЌС‚РѕРјСѓ С‡РµСЂРµР· stream
+            args.push(number);
+        }
+
+        std::cout.setf(std::ios::fixed);
+        std::cout.precision(4); // С‡РёСЃР»Рѕ СЃРёРјРІРѕР»РѕРІ РїРѕСЃР»Рµ С‚РѕС‡РєРё
+
+        while (!args.empty()) {
+            std::cout << sqrt(args.top()) << std::endl;
+            args.pop();
+        }
 
     return 0;
 }
-
-
